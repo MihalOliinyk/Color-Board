ea11d6bda67bf7d2855124d72d1e3cc39c1eb314
Myhailo Oliinyk <110667857+MihalOliinyk@users.noreply.github.com>
2022-12-03 11:46:46 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const board = document.querySelector('#board');
+const colors = [
+  '#7ad8eb',
+  '#4796a6',
+  '#3ba3b8',
+  '#135663',
+  '#18adc9',
+  '#549dd1',
+  '#22699c',
+  '#053659',
+];
+const SQUARES_NUMBER = 800;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+
+  square.addEventListener('mouseleave', () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
